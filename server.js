dc7779a9d5e7e774d0f77913413a6a34fb3806d9
melaniem <melaniem@INmelaniem.local>
2015-02-18 10:39:39 +0530

    Melanie | Adding angular dependency

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express'),
  app = express();
-app.use('/img',express.static(path.join(__dirname, 'public/images')));
-app.use('/js',express.static(path.join(__dirname, 'public/javascripts')));
-app.use('/css',express.static(path.join(__dirname, 'public/stylesheets')));
+var path = require('path');
+app.use(express.static(path.join(__dirname,'node_modules')));
 
 app.get('/', function(req, res){
   res.sendFile('/my_app/views/index.html');
